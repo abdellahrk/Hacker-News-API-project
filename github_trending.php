528570cebc60218cd5ddd26fe8edf6e9d01b19e4
giantthinker <ramadanabdel24@gmail.com>
2019-05-06 17:21:48 +0200

    Refactor code

--- github_trending.php
+++ github_trending.php
@@ -4,6 +4,16 @@ include('header.php');
 ?>
 
 <h1>Top 25 Trending Projects on GitHub</h1>
+<label for="list-of-languages">Languages:</label>
+<input list="languages-list" id="languages-list-id" name="list-of-languages" />
+
+<datalist id="languages-list">
+    <option value="Chocolate">
+    <option value="Coconut">
+    <option value="Mint">
+    <option value="Strawberry">
+    <option value="Vanilla">
+</datalist>
 <div class="row">
 
 <?php
@@ -16,8 +26,9 @@ foreach($results as  $result) {
     <div class="card" style="width: 20rem;">
         <div class="card-body">
             <h5 class="card-title"><?php echo $result->name; ?></h5>
-            <h6 class="card-subtitle mb-2 text-muted">by: <?php echo $result->author; ?></h6>
+            <h6 class="card-subtitle mb-2 text-muted">author: <?php echo $result->author; ?></h6>
             <p class="card-text"><?php echo 'Desc: ' . (substr($result->description, 0, 75)); ?></p>
+            <span href="#" class="small">Stars: <?php echo $result->stars; ?></span><br>
             <a href="<?php echo $result->url; ?>" target="_blank" class="card-link">View Project</a>
             <span href="#" class="small">Forks: <?php echo $result->forks; ?></span>
         </div>
